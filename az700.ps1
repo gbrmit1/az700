be8c3377372847e9f688c999b9adf507727f0a5b
gbrmit1 <robert.mitchell@atradius.com>
2022-11-29 12:31:49 +0000

    Initial Commit

--- az700.ps1
+++ az700.ps1
@@ -4,4 +4,4 @@ New-AzResourceGroup -Name $rg -Location northeurope
 New-AzResourceGroupDeployment `
     -Name 'RM-ARM-AZ700-Resource-Group' `
     -ResourceGroupName $rg `
-    -TemplateUri 'xxxxxxxxxxx'
+    -TemplateUri https://raw.githubusercontent.com/gbrmit1/az700/main/network1.json
