7d458395e967747e190bae65afbc0d9458f0af43
gbrmit1 <robert.mitchell@atradius.com>
2022-11-29 16:14:41 +0000

    Added 2nd network and edited ps1

--- az700.ps1
+++ az700.ps1
@@ -7,3 +7,9 @@ New-AzResourceGroupDeployment `
 -ResourceGroupName $rg `
 -TemplateUri https://raw.githubusercontent.com/gbrmit1/az700/main/network1.json `
 -TemplateParameterUri https://raw.githubusercontent.com/gbrmit1/az700/main/network1.parameters.json
+
+New-AzResourceGroupDeployment `
+-Name 'RM-ARM-AZ700-Resource-Group' `
+-ResourceGroupName $rg `
+-TemplateUri https://raw.githubusercontent.com/gbrmit1/az700/main/network1.json `
+-TemplateParameterUri https://raw.githubusercontent.com/gbrmit1/az700/main/network2.parameters.json
