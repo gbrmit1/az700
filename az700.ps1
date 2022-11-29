28765bdde252f8335bed601eefa94312720355c1
gbrmit1 <robert.mitchell@atradius.com>
2022-11-29 14:05:22 +0000

    Edited Powershell

--- az700.ps1
+++ az700.ps1
@@ -6,3 +6,4 @@ New-AzResourceGroupDeployment `
     -ResourceGroupName $rg `
     -TemplateUri https://raw.githubusercontent.com/gbrmit1/az700/main/network1.json
     -TemplateParameterUri https://raw.githubusercontent.com/gbrmit1/az700/main/network1.parameters.json
+    
