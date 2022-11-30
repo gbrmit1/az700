a608b069d5b912ba0c1088b6c2284009a69f05ef
gbrmit1 <robert.mitchell@atradius.com>
2022-11-30 11:33:08 +0000

    updated id netowrk link for privateDnsZone

--- az700.ps1
+++ az700.ps1
@@ -8,17 +8,17 @@ New-AzResourceGroupDeployment `
 -TemplateUri https://raw.githubusercontent.com/gbrmit1/az700/main/network1.json `
 -TemplateParameterUri https://raw.githubusercontent.com/gbrmit1/az700/main/network1.parameters.json
 
-New-AzResourceGroupDeployment `
--Name 'RM-ARM-AZ700-Resource-Group' `
--ResourceGroupName $rg `
--TemplateUri https://raw.githubusercontent.com/gbrmit1/az700/main/network1.json `
--TemplateParameterUri https://raw.githubusercontent.com/gbrmit1/az700/main/network2.parameters.json
+#New-AzResourceGroupDeployment `
+#-Name 'RM-ARM-AZ700-Resource-Group' `
+#-ResourceGroupName $rg `
+#-TemplateUri https://raw.githubusercontent.com/gbrmit1/az700/main/network1.json `
+#-TemplateParameterUri https://raw.githubusercontent.com/gbrmit1/az700/main/network2.parameters.json
 
-New-AzResourceGroupDeployment `
--Name 'RM-ARM-AZ700-Resource-Group' `
--ResourceGroupName $rg `
--TemplateUri https://raw.githubusercontent.com/gbrmit1/az700/main/network4.json `
--TemplateParameterUri https://raw.githubusercontent.com/gbrmit1/az700/main/network4.parameters.json
+#New-AzResourceGroupDeployment `
+#-Name 'RM-ARM-AZ700-Resource-Group' `
+#-ResourceGroupName $rg `
+#-TemplateUri https://raw.githubusercontent.com/gbrmit1/az700/main/network4.json `
+#-TemplateParameterUri https://raw.githubusercontent.com/gbrmit1/az700/main/network4.parameters.json
 
 New-AzResourceGroupDeployment `
 -Name 'RM-ARM-AZ700-Resource-Group' `
