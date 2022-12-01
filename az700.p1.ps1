138f236ab5069b31486b787d08266a79d447b8e2
gbrmit1 <robert.mitchell@atradius.com>
2022-12-01 16:16:51 +0000

    Edited Linked Template File

---
+++ az700.p1.ps1
@@ -0,0 +1,9 @@
+$rg = 'RM-ARM-AZ700'
+
+New-AzResourceGroup -Name $rg -Location northeurope
+
+New-AzResourceGroupDeployment `
+-Name 'RM-ARM-AZ700-Resource-Group' `
+-ResourceGroupName $rg `
+-TemplateUri https://raw.githubusercontent.com/gbrmit1/az700/main/az700-n1.json `
+-TemplateParameterUri https://raw.githubusercontent.com/gbrmit1/az700/main/network1.parameters.json
